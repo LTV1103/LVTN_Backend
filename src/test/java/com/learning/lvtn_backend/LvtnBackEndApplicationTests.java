8d9c6a287fa292e92d994beafa646cc576c744bc
LTV1103 <luongtrieuvy1103@gmail.com>
2025-10-28 16:23:56 +0700

    Setup Project

---
+++ src/test/java/com/learning/lvtn_backend/LvtnBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.lvtn_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LvtnBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
