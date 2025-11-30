2a7b46e16cc35e75d76ec7f99b5d63fa9e0b8b6e
LTV1103 <luongtrieuvy11032003@gmail.com>
2025-11-30 17:46:55 +0700

    return setup

--- src/main/java/com/learning/lvtn_backend/reponsitory/LessonReadingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learning.lvtn_backend.reponsitory;
-
-import com.learning.lvtn_backend.entity.LessonReading;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LessonReadingRepository extends JpaRepository<LessonReading, Integer> {
-}
--- src/main/java/com/learning/lvtn_backend/reponsitory/TestQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learning.lvtn_backend.reponsitory;
-
-import com.learning.lvtn_backend.entity.TestQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestQuestionRepository extends JpaRepository<TestQuestion, Integer> {
-}
